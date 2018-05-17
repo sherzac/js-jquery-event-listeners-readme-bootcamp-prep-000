fcedf8f5b022baf5f1246df338b33a8b705f8ebb
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-17 16:42:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,8 +21,8 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function() {
-    if ($("input:first").val() === "correct") {
-      alert('your form is going to be submitted now');
+    if ($( "input:first" ).val() === "correct") {
+      alert('Your form is going to be submitted now.');
       return
     }
   });
