387f7e7cd2150951cf20316a243b5f4442f54081
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-17 16:42:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,8 +23,9 @@ function submitIt() {
   $("form").on("submit", function() {
     if ($( "input:first" ).val() === "correct") {
       alert('Your form is going to be submitted now.');
-      return
+      return;
     }
+    return;
   });
 }
 
